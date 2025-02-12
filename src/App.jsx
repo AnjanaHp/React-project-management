df59fcf67eb6d23396120c51d27b3c5db804add0
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-02-12 11:34:28 +0100

    Implement functionality to create new project

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,23 @@ import { Route, Routes } from "react-router-dom"
 import Navbar from "./Components/Navbar"
 import HomePage from "./Pages/HomePage"
 import ProjectListPage from "./Pages/ProjectListPage"
+import CreateProjectPage from "./Pages/CreateProjectPage"
+
+
 
 function App() {
 
 
   return (
     <>
-     <Navbar />
+      <Navbar />
 
-       <Routes>
-<Route path="/" element = {<HomePage />}/>
-<Route path="/projects" element = {<ProjectListPage />}/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<ProjectListPage />} />
+        <Route path="/projects/create" element={<CreateProjectPage />} />
+      </Routes>
 
-  </Routes>
-  
     </>
   )
 }
--- src/Pages/CreateProjectPage.jsx
+++ src/Pages/CreateProjectPage.jsx
@@ -1,16 +1,55 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { API_URL } from "../config/api";
+import { useNavigate } from "react-router-dom";
 
 
-
-function CreateProjectPage(){
-
-
-
-    return(
-<>
+function CreateProjectPage() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const navigate = useNavigate();
 
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-</>
+        const newProject = {
+            title: title,
+            description: description
+        }
+        // axios.post(url, data)
+        axios.post(`${API_URL}/projects`, newProject)
+            .then((response) => {
+                console.log("Sucess");
+                navigate("/projects");
+            })
+            .catch(e => console.log("Error"));
+
+    }
+
+
+    return (
+        <div className="CreateProjectPage">
+            <h3>Add Project </h3>
+            <form onSubmit={handleSubmit} >
+                <label> Title:
+                    <input type="text"
+                        name="title"
+                        placeholder="enter the title"
+                        onChange={(e) => { setTitle(e.target.value) }} />
+
+                </label>
+
+                <label> Title:
+                    <input type="text"
+                        name="dexcription"
+                        placeholder="enter the description"
+                        onChange={(e) => { setDescription(e.target.value) }} />
+                    <button>Create</button>
+                </label>
+            </form>
+
+        </div>
     );
 }
 
